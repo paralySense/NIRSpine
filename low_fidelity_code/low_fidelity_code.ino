acc2058faae373877b4874c51678f22687ac64ef
Daniel Gan <danielgan2019@gmail.com>
2025-02-08 18:58:22 -0600

    modified this code a bit when running experiments

--- low_fidelity_code/low_fidelity_code.ino
+++ low_fidelity_code/low_fidelity_code.ino
@@ -1,15 +1,23 @@
-#define LED_PIN 8
+#define LED_PINBLUE 5
+#define LED_PINRED 8
 #define SENSOR_PIN A5
 
 void setup() {
 Serial.begin(9600);
-pinMode(LED_PIN, OUTPUT); 
+pinMode(LED_PINBLUE, OUTPUT); 
+pinMode(LED_PINRED, OUTPUT); 
 pinMode(SENSOR_PIN, INPUT);
+
+Serial.println("Time, Sensor Value");
 }
 
 void loop() {
-digitalWrite(LED_PIN, LOW);
+digitalWrite(LED_PINBLUE, HIGH);
+digitalWrite(LED_PINRED, HIGH);
 int sensorValue = analogRead(SENSOR_PIN);
+//float time = millis();
+// Serial.print(millis());
+// Serial.print(",");
 Serial.println(sensorValue);
-delay(10);
+delay(50);
 }
